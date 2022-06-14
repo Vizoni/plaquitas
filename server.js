b7ba8732b1f3bbadb557356dcc2787a361e15a8d
Raphael Vizoni do Prado <ravizoni@gmail.com>
2022-06-13 22:02:04 -0300

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use("/", express.static(resolve(__dirname, "./dist")));
 app.get("*", function (req, res) {
-	res.sendFile(resolve(__dirname, "./dist").join("/index.html"));
+	res.sendFile(resolve(__dirname, "./dist", "/index.html"));
 });
 
 app.listen(process.env.PORT || 3000, (err) => {
