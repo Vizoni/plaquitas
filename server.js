2cee560a83812e34a01170bf76f7636ebe9523d6
Raphael Vizoni do Prado <ravizoni@gmail.com>
2022-06-13 21:17:40 -0300

    fix routes on heroku

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require("express");
 
 console.log("Starting server...");
 
+const path = require("path");
 const { resolve } = require("path");
 const app = express();
 
 app.use("/", express.static(resolve(__dirname, "./dist")));
 
-app.listen(process.env.PORT || 3000, () => {
-	if (err) {
-		return console.log("ERRO", err);
-	} else {
-		console.log("Tudo funcionando certinho");
-	}
+app.get("/*", (req, res) => {
+	console.log("gettttttt --- ", path.join(__dirname, "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(process.env.PORT || 3000, (err) => {
+	err ? console.log("error", err) : console.log("Funcionando...");
 });
