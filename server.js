328e7a9934c9b18d1cfd01bd85fda847c4443648
Raphael Vizoni do Prado <ravizoni@gmail.com>
2022-06-13 21:33:55 -0300

    trying to fix routes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const { resolve } = require("path");
 const app = express();
 
 app.use("/", express.static(resolve(__dirname, "./dist")));
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 app.listen(process.env.PORT || 3000, (err) => {
 	if (err) {
