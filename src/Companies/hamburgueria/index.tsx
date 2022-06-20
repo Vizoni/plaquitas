3046dca25ed8d8c18dbef772bf5d2a4f8dcef30f
Raphael Vizoni do Prado <ravizoni@gmail.com>
2022-06-19 23:01:23 -0300

    fix: skeleton to burguer page

--- src/Companies/hamburgueria/index.tsx
+++ src/Companies/hamburgueria/index.tsx
@@ -1,26 +1,39 @@
-import React, { lazy } from "react";
-import { Container, Container2, Container3 } from "./styles";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-function PadariaExemplo() {
-	// const image1: any = lazy(() => import("./companies/hamburgueria/1.png"));
-	// const image2: any = lazy(() => import("./companies/hamburgueria/2.png"));
-	// const image3: any = lazy(() => import("./companies/hamburgueria/3.png"));
+function HamburgueriaExemplo() {
 	const image1: any = "./companies/hamburgueria/1.webp";
 	const image2: any = "./companies/hamburgueria/2.webp";
 	const image3: any = "./companies/hamburgueria/3.webp";
 
+	const SkeletonComponent = (
+		<Skeleton count={1} style={{ margin: "0.5rem" }} height={"30vh"} />
+	);
+
 	return (
 		<>
-			{/* <Container />
-			<Container2 />
-			<Container3 /> */}
-			<LazyLoadImage src={image1} threshold={100} effect={"blur"} />
-			<LazyLoadImage src={image2} threshold={100} effect={"blur"} />
-			<LazyLoadImage src={image3} threshold={100} effect={"blur"} />
+			<LazyLoadImage
+				src={image1}
+				threshold={100}
+				effect={"blur"}
+				placeholder={SkeletonComponent}
+			/>
+			<LazyLoadImage
+				src={image2}
+				threshold={100}
+				effect={"blur"}
+				placeholder={SkeletonComponent}
+			/>
+			<LazyLoadImage
+				src={image3}
+				threshold={100}
+				effect={"blur"}
+				placeholder={SkeletonComponent}
+			/>
 		</>
 	);
 }
 
-export default PadariaExemplo;
+export default HamburgueriaExemplo;
